193a5d6234954aa9e9f98d31a82d49471b986746
EE539 <56602339+EE539@users.noreply.github.com>
2022-07-24 16:55:51 +0300

    A bit level design :sparkle:

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor; // [SerializeField] [Range(0,1)] 
+    [SerializeField] float period = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(period == 0) { return; }
+        float cycles = Time.time / period; //Continually growing over time
+        const float tau = Mathf.PI * 2; //const valueof 6.283 ...
+
+        float rawSinWave = Mathf.Sin(cycles * tau);//going from -1 to 1
+        movementFactor = (rawSinWave + 1f) / 2f; //recalculate to go from 0 to 1 so its cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
