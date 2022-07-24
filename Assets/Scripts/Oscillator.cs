00b3ab9a254620d2acd3448e0f9a70bca000ae83
EE539 <56602339+EE539@users.noreply.github.com>
2022-07-24 18:31:11 +0300

    Add more levelss

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -17,7 +17,7 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(period == 0) { return; }
+        if(period <= Mathf.Epsilon) { return; }
         float cycles = Time.time / period; //Continually growing over time
         const float tau = Mathf.PI * 2; //const valueof 6.283 ...
 
